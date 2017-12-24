86fccaafc14a0ea7b75bd1393655abbee4cd8123
Nick Hartjes <info@nickhartjes.nl>
2017-12-24 15:22:56 +0100

    Putting some data in folders

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 HAN BIS C | INFSYS | Gameparadise DO
+
+### Pre:
+GameParadise-DDL.sql
+GameParadise-INSERT.sql
+
+### Opdracht 1:
+GP-DML-SCRIPT.sql
+
+### Opdracht 2:
+GP-DDL-SCRIPT-CONSTRAINTS.sql
+
+### Opdracht 3:
+GP-DDL-UPDATE-DELETE-SCRIPT.sql
+
+### Opdracht 4:
+GP_TESTSCRIPT.sql
+
+### Opdracht 5:
+-
+
+### Opdracht 5:
+GP_QUERY-SCRIPT.sql
---
+++ sql/GP_TESTSCRIPT-NickHartjes.sql
@@ -0,0 +1,9 @@
+/*================================================================*/
+/* Database name:  GameParadise			                          */
+/* DBMS name:      Microsoft SQL Server 2017                      */
+/* Created on:     20/12/2017							          */
+/* Made by:        Martijn de Geus, Nick Hartjes en Joey Stoffels */
+/*================================================================*/
+
+USE GAMEPARADISE
+GO
